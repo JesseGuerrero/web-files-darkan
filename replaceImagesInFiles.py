0369f3adf5de5d964f0ec4fc00b0fe84c72b9e55
Jesse Guerrero <jesseguerrero1991@gmail.com>
2023-03-17 21:03:16 -0500

    Updated for image replacement

--- replaceImagesInFiles.py
+++ replaceImagesInFiles.py
@@ -1,19 +1,49 @@
 import os
+from pathlib import Path
 images = os.listdir("./2012")
 
-file = "world"
-linesRead = []
-linesWrite = []
+path = os.path.abspath("D:/Other/RS2012 Website/2012 Website/public")
 
-# string to search in file
-with open(f'{file}.hbs', 'r') as fp:
-    # read all lines using readline()
-    linesRead = fp.readlines()
-    for row in linesRead:
-        for image in images:
-            if row.find(image) != -1:
-                row = row.replace(row.split(image)[0].split("\"")[-1], "https://raw.githubusercontent.com/JesseGuerrero/web-files-darkan/master/2012/")
-                print('line:', row, end="")
-        linesWrite.append(row)
-with open(f'{file}2.hbs', 'w') as fp:
-    fp.writelines(linesWrite)
+def walkDirectory(root):
+    filePaths = list(Path(root).rglob("*"))
+    result = []
+    # for filePath in filePaths:
+    #     if ".git" in str(filePath) or ".idea" in str(filePath) or "2012 Website\\bin" in str(filePath) \
+    #             or "node_modules" in str(filePath) or ".scss" in str(filePath) or ".css" in str(filePath):
+    #         continue
+    for filePath in filePaths:
+        if ".scss" in str(filePath):
+            result.append(filePath)
+    return result
+count = 0
+for absPathFile in walkDirectory(path):
+    linesRead = []
+    linesWrite = []
+    # string to search in file
+    if(os.path.isfile(absPathFile)):
+        with open(f'{absPathFile}', 'r') as fp:
+            # read all lines using readline()
+            try:
+                linesRead = fp.readlines()
+                for row in linesRead:
+                    for image in images:
+                        if row.find("/" + image) != -1:
+                            first_half = row.split(image)[0]
+                            reversed_first_half = first_half[::-1]
+                            splitter = "\""
+                            for c in reversed_first_half:
+                                if c == "\'":
+                                    splitter = "\'"
+                                    break
+                                elif c == "(":
+                                    splitter = "("
+                            row = row.replace(row.split(image)[0].split(splitter)[-1], "https://raw.githubusercontent.com/JesseGuerrero/web-files-darkan/master/2012/")
+                            print(f'{absPathFile}, line:{row}', end="")
+                            count +=1
+                    linesWrite.append(row)
+            except:
+                pass
+        with open(f'{absPathFile}', 'w') as fp:
+            fp.writelines(linesWrite)
+print(f'count: {count}')
+#from last '/' check image name
